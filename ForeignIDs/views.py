71040d13847cbc324906d3188076b3b3d4074c5a
Андрій Сокульський <71042762+AndriiSokulskyi@users.noreply.github.com>
2020-12-02 16:56:02 +0200

    Update views.py

--- ForeignIDs/views.py
+++ ForeignIDs/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .serializers import ForeignIDDetailSerializer
 from .models import ForeignID
-from rest_framework import filters
+from rest_framework import filters, pagination
 
 # Create your views here.
 
@@ -12,6 +12,7 @@ class ForeignIDCollectionView(generics.ListAPIView):
     serializer_class = ForeignIDDetailSerializer
     queryset = ForeignID.objects.all()
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    pagination_class = pagination.LimitOffsetPagination
     search_fields = ['id', 'country_code', 'passport_no', 'first_name', 'last_name', 'date_of_birth', 'date_of_issue', 'date_of_expire']
 
 class ForeignIDDetailView(generics.RetrieveUpdateDestroyAPIView):
