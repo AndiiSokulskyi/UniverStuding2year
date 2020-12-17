aeb518c47a7f149a7cc5f702f14532834ce06e83
Андрій Сокульський <71042762+AndriiSokulskyi@users.noreply.github.com>
2020-12-17 03:16:10 +0200

    Add files via upload

---
+++ EducPract8/ForegnID/views.py
@@ -0,0 +1,26 @@
+from rest_framework import generics
+from .serializers import ForeignIDDetailSerializer
+from .models import ForeignID
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import filters, pagination
+
+# Create your views here.
+
+class ForeignIDCreateView(generics.CreateAPIView):
+    serializer_class = ForeignIDDetailSerializer
+    #permission_classes = (IsAuthenticated, )
+
+class ForeignIDCollectionView(generics.ListAPIView):
+    serializer_class = ForeignIDDetailSerializer
+    queryset = ForeignID.objects.all()
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    pagination_class = pagination.LimitOffsetPagination
+    search_fields = ['id', 'country_code', 'passport_no', 'first_name', 'last_name', 'date_of_birth', 'date_of_issue', 'date_of_expire']
+    #permission_classes = (IsAdminUser, )
+
+class ForeignIDDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ForeignIDDetailSerializer
+    queryset = ForeignID.objects.all()
+    #permission_classes = (IsOwnerOrReadOnly, )
+
